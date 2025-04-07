6398b1ce4b55cb0fadfe8f12b087418c5a0cc637
Akeri <samaescii@gmail.com>
2025-04-06 21:27:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const result = invitadospareja(name);
 
 function invitadospareja(names) {
     let parejas = []; 
-    for (let i = 0; i < names.length - 1; i++) {  //penúltimo elemento
-        for (let j = i + 1; j < names.length; j++) {  //comparación con próximos elementos 
-            
-            if (names[i][0] === names[j][0]) {//Coinciden
+    
+    for (let i = 0; i < names.length - 1; i++) {  // Recorrer hasta el penúltimo elemento
+        for (let j = i + 1; j < names.length; j++) {  // Comparar con los siguientes elementos
+            if (names[i][0] === names[j][0]) {  // Si las iniciales coinciden
                 parejas.push([names[i], names[j]]);
             }
         }
     }
-    return parejas; 
+    return parejas;
 }
 
 if (result.length > 0) {
